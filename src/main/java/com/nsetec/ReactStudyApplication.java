f253c58761bb5534d413f266d8eeb80cd60d0f8b
sjkim1482 <monochrose94@gmail.com>
2021-06-07 09:09:17 +0900

    - React를 이용한 화면개발 공부

---
+++ src/main/java/com/nsetec/ReactStudyApplication.java
@@ -0,0 +1,13 @@
+package com.nsetec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactStudyApplication.class, args);
+	}
+
+}
